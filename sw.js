2a6cf9ed5d5f676ceaf279e3e4bd241424058fd5
Hung-Te Lin <hungte@gmail.com>
2026-03-19 00:03:29 +0800

    Update msg and uprev

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service Worker for PWA. Must be in root folder, and do not cache itself. */
 
-const CACHE_NAME = 'epub-s2tw-v1.9';
+const CACHE_NAME = 'epub-s2tw-v1.10';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
